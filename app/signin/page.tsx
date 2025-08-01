55cd8cca134c263f00bcfeed99334955bb6d98cf
Herb Segismar <herbert.segismar@yahoo.com>
2025-08-02 07:17:31 +1200

    sign in card

--- app/signin/page.tsx
+++ app/signin/page.tsx
@@ -1,14 +1,45 @@
 export default function SignIn() {
     return (
-        <div>
-            <div>
-                <div>
-                    <h1>Personalized AI Newsletter</h1>
-                </div>
-                <div>
-
-                </div>
-            </div>
+      <div className="twcontainer">
+        <div className="bg-slate-100 shadow-black/50 shadow-md border border-blue-100 rounded-xl w-80 md:w-[500px] h-64 md:h-[300px] twflex flex-col">
+          <div>
+            <h1 className="text-amber-500 text-2xl md:text-4xl">
+              Personalized AI Newsletter
+            </h1>
+            <p className=" text-blue-400 font-extralight text-center text-lg md:text-xl mb-5">
+              Sign-in to your Account
+            </p>
+          </div>
+          <div className="twflex">
+            <form action="">
+              <div className="mb-2 twflex gap-5">
+                <label>Email Address</label>
+                <input
+                  id="email"
+                  name="email"
+                  type="email"
+                  required
+                  placeholder="Fill in your email address"
+                  className="border border-blue-300 rounded-sm w-48 text-center text-md"
+                />
+              </div>
+              <div className="mb-5 twflex gap-5">
+                <label>Password</label>
+                <input
+                  id="password"
+                  name="password"
+                  type="password"
+                  required
+                  placeholder="Enter password"
+                  className="border border-blue-300 rounded-sm w-48 text-center text-md"
+                />
+              </div>
+              <div className="twflex mx-auto bg-blue-500 text-slate-100 w-20 h-10 rounded-lg font-normal">
+                <button type="submit">Sign In</button>
+              </div>
+            </form>
+          </div>
         </div>
-    )
+      </div>
+    );
 }
