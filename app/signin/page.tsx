ab2395d8044596f74fd4ccd75a82d06f2bb3a3c5
Herb Segismar <herbert.segismar@yahoo.com>
2025-08-02 05:58:42 +1200

    signin dashboard icon

---
+++ app/signin/page.tsx
@@ -0,0 +1,14 @@
+export default function SignIn() {
+    return (
+        <div>
+            <div>
+                <div>
+                    <h1>Personalized AI Newsletter</h1>
+                </div>
+                <div>
+
+                </div>
+            </div>
+        </div>
+    )
+}
