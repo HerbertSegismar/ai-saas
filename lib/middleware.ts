581d664c3ed74c07cf21601fcc896ee4bb976af7
Herb Segismar <herbert.segismar@yahoo.com>
2025-08-02 19:22:20 +1200

    errors midware

--- lib/middleware.ts
+++ lib/middleware.ts
@@ -51,28 +51,16 @@ if (request.nextUrl.pathname === "/signin" && user) {
   return NextResponse.redirect(url);
 } 
 
-if (request.nextUrl.pathname === "/" && !user) {
+if (
+  !user &&
+  !request.nextUrl.pathname.startsWith("/signin") &&
+  !request.nextUrl.pathname.startsWith("/auth")
+) {
+  // no user, potentially respond by redirecting the user to the login page
   const url = request.nextUrl.clone();
   url.pathname = "/signin";
   return NextResponse.redirect(url);
-} 
-
-if (request.nextUrl.pathname === "/" && !user) {
-  const url = request.nextUrl.clone();
-  url.pathname = "/signin";
-  return NextResponse.redirect(url);
-} 
-
-//   if (
-//     !user &&
-//     !request.nextUrl.pathname.startsWith("/login") &&
-//     !request.nextUrl.pathname.startsWith("/auth")
-//   ) {
-//     // no user, potentially respond by redirecting the user to the login page
-//     const url = request.nextUrl.clone();
-//     url.pathname = "/login";
-//     return NextResponse.redirect(url);
-//   }
+}
 
   // IMPORTANT: You *must* return the supabaseResponse object as it is.
   // If you're creating a new response object with NextResponse.next() make sure to:
